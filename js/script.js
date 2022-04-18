346fc549f0824be883aa14fc1a9a80d23bd5481a
MatheusV.A <ananiasmatheus22@outlook.com>
2022-04-18 17:07:47 -0300

    up

--- js/script.js
+++ js/script.js
@@ -2,19 +2,35 @@ const btnMobile = document.getElementById('btn-mobile');
 function toggleMenu(){
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
-
+	
+	
 }
 btnMobile.addEventListener('click', toggleMenu);
 
-$('.nav a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			
-	$('html, body, main').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
+
+const $menu = document.getElementById('nav');
+const $btnMenu1 = document.querySelector('.btnMenu1');
+const $btnMenu2 = document.querySelector('.btnMenu2');
+const $btnMenu3 = document.querySelector('.btnMenu3');
+const $btnMenu4 = document.querySelector('.btnMenu4');
+
+$btnMenu1.addEventListener('click', function () {
+	$menu.classList.remove('active')
 });
+
+$btnMenu2.addEventListener('click', function () {
+	$menu.classList.remove('active')
+});
+
+$btnMenu3.addEventListener('click', function () {
+	$menu.classList.remove('active')
+});
+
+$btnMenu4.addEventListener('click', function () {
+	$menu.classList.remove('active')
+});
+
+
 
 
 
